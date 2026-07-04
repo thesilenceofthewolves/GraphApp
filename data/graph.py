ee30a163f28b32d525ffbef98207c2dd2e5fef18
ousman <83393758+thesilenceofthewolves@users.noreply.github.com>
2026-07-04 15:19:20 +0100

    Refactor Graph class for improved readability

--- data/graph.py
+++ data/graph.py
@@ -1,34 +1,46 @@
 from azure.identity.aio import ClientSecretCredential
 from msgraph import GraphServiceClient
-from msgraph.generated.users.users_request_builder import UsersRequestBuilder 
+from msgraph.generated.users.users_request_builder import UsersRequestBuilder
+
 
 class Graph:
     def __init__(self, config):
-        client_id = config['clientId']
-        tenant_id = config['tenantId']
-        client_secret = config['clientSecret']
+        client_id = config["clientId"]
+        tenant_id = config["tenantId"]
+        client_secret = config["clientSecret"]
 
         self.client_credential = ClientSecretCredential(
-            tenant_id, client_id, client_secret
+            tenant_id=tenant_id,
+            client_id=client_id,
+            client_secret=client_secret,
         )
+
         self.app_client = GraphServiceClient(self.client_credential)
 
     async def get_app_only_token(self):
-        graph_scope = 'https://graph.microsoft.com/.default'
+        graph_scope = "https://graph.microsoft.com/.default"
+
+        # This will raise an exception if authentication fails.
         access_token = await self.client_credential.get_token(graph_scope)
+
         return access_token.token
 
     async def get_users(self):
         query_params = UsersRequestBuilder.UsersRequestBuilderGetQueryParameters(
-            select=['displayName', 'id', 'mail'],
+            select=["displayName", "id", "mail"],
             top=25,
-            orderby=['displayName']
+            orderby=["displayName"],
         )
-        request_config = UsersRequestBuilder.UsersRequestBuilderGetRequestConfiguration(
-            query_parameters=query_params
+
+        request_config = (
+            UsersRequestBuilder.UsersRequestBuilderGetRequestConfiguration(
+                query_parameters=query_params
+            )
         )
-        users = await self.app_client.users.get(request_configuration=request_config)
-        return users
+
+        return await self.app_client.users.get(
+            request_configuration=request_config
+        )
 
     async def make_graph_call(self):
         org = await self.app_client.organization.get()
